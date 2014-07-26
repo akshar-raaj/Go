a1874de2188cc4adb6f92bc65fbd905265ea3360
Akshar Raaj <akshar@agiliq.com>
2014-07-27 00:18:38 +0530

    Euler 2

---
+++ 2.go
@@ -0,0 +1,21 @@
+/*By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.*/
+
+package main
+
+import "fmt"
+
+var MAX = 4000000
+
+func main() {
+    var first, second = 1, 2
+    var third = first + second
+    var sum = 2
+    for third < MAX {
+        if (third % 2 == 0) {
+            sum += third
+        }
+        first, second = second, third
+        third = first + second
+    }
+    fmt.Println(sum)
+}
