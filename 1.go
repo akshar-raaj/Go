b3e8ce6c766291a6f7f2660959f67b252206ff65
Akshar Raaj <akshar@agiliq.com>
2014-07-27 12:39:35 +0530

    Ran go fmt

--- 1.go
+++ 1.go
@@ -1,15 +1,15 @@
 /*Find the sum of all the multiples of 3 or 5 below 1000.*/
 
-package main 
+package main
 
 import "fmt"
 
 func main() {
-    var sum = 0
-    for i := 0; i < 1000; i++ {
-        if (i % 3 == 0 || i % 5 == 0) {
-            sum += i;
-        }
-    }
-    fmt.Println(sum)
+	var sum = 0
+	for i := 0; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	fmt.Println(sum)
 }
--- 2.go
+++ 2.go
@@ -7,15 +7,15 @@ import "fmt"
 var MAX = 4000000
 
 func main() {
-    var first, second = 1, 2
-    var third = first + second
-    var sum = 2
-    for third < MAX {
-        if (third % 2 == 0) {
-            sum += third
-        }
-        first, second = second, third
-        third = first + second
-    }
-    fmt.Println(sum)
+	var first, second = 1, 2
+	var third = first + second
+	var sum = 2
+	for third < MAX {
+		if third%2 == 0 {
+			sum += third
+		}
+		first, second = second, third
+		third = first + second
+	}
+	fmt.Println(sum)
 }
