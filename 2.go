39937f547ca61fd28272c3cce292691ec024544b
Akshar Raaj <akshar@agiliq.com>
2015-02-21 20:45:42 +0530

    made Euler2 clearer

--- 2.go
+++ 2.go
@@ -1,21 +1,31 @@
-/*By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.*/
+/*By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms. Answer: 4613732*/
 
 package main
 
 import "fmt"
 
 const MAX = 4000000
+var first = 1
+var second = 2
+
+func is_even(num int) bool {
+	return num % 2 == 0
+}
 
 func main() {
-	first, second := 1, 2
-	third := first + second
-	sum := 2
-	for third < MAX {
-		if third%2 == 0 {
+	sum := 0
+	if is_even(first) {
+		sum += first
+	}
+	if is_even(second) {
+		sum += second
+	}
+	for (first + second) < MAX {
+		third := first+second
+		if is_even(third) {
 			sum += third
 		}
 		first, second = second, third
-		third = first + second
 	}
 	fmt.Println(sum)
 }
